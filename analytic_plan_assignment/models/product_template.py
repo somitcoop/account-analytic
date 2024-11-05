929d57aa94320854030c6431813b4478f9a4a8e6
Nicolás Ramos <nicolas.ramos@somit.coop>
2024-11-05 15:48:41 +0100

    Cambio propuestos en reunión

--- analytic_plan_assignment/models/product_template.py
+++ analytic_plan_assignment/models/product_template.py
@@ -5,4 +5,10 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    analytic_plan_id = fields.Many2one("account.analytic.plan", string="Plan Analítico")
+    analytic_plan_id = fields.Many2one(
+        "account.analytic.plan",
+        string="Analytic Plan",
+        help="When creating a project from this product, this analytic plan will be automatically "
+             "assigned to the project's analytic account. This is useful for automatically "
+             "categorizing and organizing your project's analytical accounting."
+    )
